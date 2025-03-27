b5ac5ffa6bcc16dcbe758220542fe60765277476
YelloNox <alecmurphy0@gmail.com>
2025-03-27 05:54:31 +0000

    Added stickerpack and updated compose

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '40efe954'
-version = '0.1.0+dev.40efe954'
-linkified_version = '0.1.0+dev.[40efe954](https://github.com/maunium/stickerpicker/commit/40efe954c63b6a5a3cc49a2fd5121254928bb93d)'
+git_revision = 'aec10073'
+version = '0.1.0+dev.aec10073'
+linkified_version = '0.1.0+dev.[aec10073](https://github.com/maunium/stickerpicker/commit/aec10073496e22958f4fa764da30f3919fee3586)'
